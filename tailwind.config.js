4d48be6856e8f50f69b617d9acb0a2d0f4a73e1f
Denis-Turbatu <turbatudenis81@gmail.com>
2024-08-10 15:21:03 +0200

    feat: installed Tailwind CLI + calculator design

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "raisin": "#202020",
+        "picton": "#47b1e8",
+        "light-picton": "#47ccff",
+        "charcoal": "#323232",
+        "black-olive": "#3c3c3c",
+      }
+    }
+  },
+  plugins: []
+};
